0358cf19a277c3fc4f127338720ef365d71b9b36
Maike <maike_jbe@hotmail.com>
2018-11-12 09:27:44 -0300

    Criar classe de Pessoa Física

---
+++ sources/Natural.php
@@ -0,0 +1,71 @@
+<?php
+namespace Ciebit\Persons;
+
+use Ciebit\Persons\Enum\EducationalLevel;
+use Ciebit\Persons\Enum\Gender;
+use Ciebit\Persons\Enum\MaritalStatus;
+use DateTime;
+
+class Natural extends Person
+{
+    private $birthDate; #DateTime
+    private $educationalLevel; #EducationalLevel
+    private $gender; #Gender
+    private $maritalStatus; #MaritalStatus
+    private $nickname; #string
+
+    public function getBirthDate(): DateTime
+    {
+        return $this->birthDate;
+    }
+
+    public function getEducationalLevel(): EducationalLevel
+    {
+        return $this->educationalLevel;
+    }
+
+    public function getGender(): Gender
+    {
+        return $this->gender;
+    }
+
+    public function getMaritalStatus(): MaritalStatus
+    {
+        return $this->maritalStatus;
+    }
+
+    public function getNickname(): string
+    {
+        return $this->nickname;
+    }
+
+    public function setBirthDate(DateTime $birthDate): self
+    {
+        $this->birthDate = $birthDate;
+        return $this;
+    }
+
+    public function setEducationalLevel(EducationalLevel $educationalLevel): self
+    {
+        $this->educationalLevel = $educationalLevel;
+        return $this;
+    }
+
+    public function setGender(Gender $gender): self
+    {
+        $this->gender = $gender;
+        return $this;
+    }
+
+    public function setMaritalStatus(MaritalStatus $maritalStatus): self
+    {
+        $this->maritalStatus = $maritalStatus;
+        return $this;
+    }
+
+    public function setNickname(string $nickname): self
+    {
+        $this->nickname = $nickname;
+        return $this;
+    }
+}
